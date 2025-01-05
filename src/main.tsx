13f6b9eb6431e9fc03b9e1bebddaa8d3c5b6fcfb
MJB012 <jamilbashir012@gmail.com>
2025-01-06 00:10:51 +0500

    tailwind configuration

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Button />
+    <Button label="Submit" style="bg-blue-500 text-white border-none" />
   </StrictMode>
 );
 
