fbaeecd227d2da18456c29dca7c47e3dc00c24d2
Rizwan <rizwanramzan648@gmail.com>
2025-01-20 16:14:50 +0500

    Home page banner done

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,6 +1,6 @@
 import BannerImage from "../../assets/images/home1.png";
 import { Link } from "react-router-dom";
-import Button from "../Button/Button"; 
+import Button from "../Button/Button";
 import {
   BANNER_TITLE,
   BANNER_SUB_TITLE,
@@ -8,57 +8,79 @@ import {
 } from "../../site-content/SiteContent";
 const Banner = () => {
   return (
-    <section className="bg-home bg-green-950 py-20" id="home">
-      <div className="bg-overlay"></div>
-      <div className="container mx-auto px-4">
-        <div className="items-center flex">
-          <div className="grid-cols-6">
-            <div className="home-content me-lg-5">
-              <h6 className="sub-title mb-3 text-white">{BANNER_SUB_TITLE}</h6>
-              <h1 className="mb-4 text-white">
-                {BANNER_TITLE}
-                {/* <span className="text-primary">zoric</span> */}
-              </h1>
-              <p className="text-white-50 fs-17">{BANNER_DESC}</p>
-              <div className="mt-4">
-                <Link className="mr-3" to="/">
-                  <Button
-                    label="Get Started"
-                    onClick={() => alert("Button Clicked!")}
-                    className="text-slate-50 hover:bg-emerald-700 hover:border-emerald-700 bg-emerald-500 border border-emerald-500"
-                    style={{}}
-                  />
-                </Link>
-                <Link to="/">
-                  <Button
-                    label="Learn more"
-                    onClick={() => alert("Button Clicked!")}
-                    className="text-slate-50 hover:bg-white hover:text-black border border-white"
-                    style={{}}
-                  />
-                </Link>
+    <>
+      <section className="bg-home py-40 bg-cover bg-center bg-no-repeat relative" id="home">
+        <div className="bg-cyan-950 bg-overlay opacity-90 absolute left-0 right-0 bottom-0 top-0"></div>
+        <div className="container relative mx-auto px-4">
+          <div className="items-center grid grid-cols-2">
+            <div className="grid-cols-1">
+              <div className="home-content me-lg-5 w-3/4">
+                <h6 className="sub-title mb-3 text-white text-xl font-semibold">
+                  {BANNER_SUB_TITLE}
+                </h6>
+                <h1 className="mb-4 text-white text-4xl font-bold leading-normal">
+                  {BANNER_TITLE}
+                  <span className="text-primary text-emerald-500"> zoric</span>
+                </h1>
+                <p className="text-white fs-17">{BANNER_DESC}</p>
+                <div className="mt-4">
+                  <Link className="mr-3" to="/">
+                    <Button
+                      label="Get Started"
+                      onClick={() => alert("Button Clicked!")}
+                      className="bgButton"
+                      style={{}}
+                    />
+                  </Link>
+                  <Link to="/">
+                    <Button
+                      label="Learn more"
+                      onClick={() => alert("Button Clicked!")}
+                      className="bgTransparent"
+                      style={{}}
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="grid-cols-6">
-            <div className="home-dashboard mt-4 mt-lg-0">
-              <img
-                src={BannerImage}
-                alt="Home Dashboard"
-                className="img-fluid"
-                width={600}
-                height={400}
-              />
-              <div className="play-btn">
-                <Link className="image-popup" to="/">
-                  <i className="mdi mdi-motion-play"></i>
-                </Link>
+            <div className="grid-cols-1">
+              <div className="home-dashboard mt-4 mt-lg-0 py-2 px-2 relative">
+                <img
+                  src={BannerImage}
+                  alt="Home Dashboard"
+                  className="img-fluid w-100"
+                />
+                <div className="play-btn absolute top-1/2 left-1/2">
+                  <Link className="image-popup" to="/">
+                    <i className="mdi mdi-motion-play"></i>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </section>
+      <div className="relative">
+        <div className="shape absolute right-0 bottom-0 left-0">
+          <svg className="w-full"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="1440"
+            height="150"
+            preserveAspectRatio="none"
+            viewBox="0 0 1440 150"
+          >
+            <g mask='url("#SvgjsMask1022")' fill="none">
+              <path
+                d="M 0,58 C 144,73 432,131.8 720,133 C 1008,134.2 1296,77.8 1440,64L1440 250L0 250z"
+                fill="rgba(255, 255, 255, 1)"
+              ></path>
+            </g>
+          </svg>
+        </div>
       </div>
-    </section>
+    </>
   );
 };
 
